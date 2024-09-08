8e916eb6692801274fc6f5d412bd5c2c2df64908
M.T. Tasin <m.t.tasin20@gmail.com>
2024-09-08 21:13:43 +0600

    created new ecommerce project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+
+export default function App() {
+  return <MantineProvider>
+
+
+
+
+  </MantineProvider>;
+}
