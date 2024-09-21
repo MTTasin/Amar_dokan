bb92751897dbfb582d6d7080109dc8d021c4c332
M.T. Tasin <m.t.tasin20@gmail.com>
2024-09-21 21:07:17 +0600

    nothing

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -30,5 +30,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path("", include(router.urls)),
-    path('accounts/', include('allauth.urls')),
 ]
