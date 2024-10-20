16fd185b60e30a749247d6dad0bc2ab4542fdbfb
M.T. Tasin <m.t.tasin20@gmail.com>
2024-10-20 20:44:16 +0600

    A little step to fully functional Cart system

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -3,58 +3,81 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Cookies from "js-cookie";
 
-
 export default function Card(props) {
-    const [cartbutton, setCartbutton] = useState(false);
-    const [Cart, setCart] = useState([]);
+  const [cartbutton, setCartbutton] = useState(false);
 
-function addToCart() {
+  function addToCart() {
     if (cartbutton) {
-        setCartbutton(false);
+      setCartbutton(false);
+      Cookies.remove("id", props.id);
+
     } else {
-        setCartbutton(true);
+      setCartbutton(true);
+      Cookies.set("id", [props.id], { expires: 7 });
     }
   }
-    
 
-    const tags = props.tags.map((tag) => {
-        return (
-            <div key={tag} className="badge badge-outline hover:bg-yellow-300 hover:text-black ">{tag}</div>
-        )
-    })
+  useEffect(() => {
+    const storeId = Cookies.get("id");
+    if (storeId == props.id) {
+      setCartbutton(true);
+    } else {
+      setCartbutton(false);
+    }
+  }, []);
+
+  
+
+  const tags = props.tags.map((tag) => {
+    return (
+      <div
+        key={tag}
+        className="badge badge-outline hover:bg-yellow-300 hover:text-black "
+      >
+        {tag}
+      </div>
+    );
+  });
 
   return (
     <div className="card bg-base-900 w-96 shadow-xl hover:scale-105 hover:transition hover:duration-200 hover:ease-in z-0">
       <figure>
-        <img
-          src={props.img}
-          alt={props.title}
-        />
+        <img src={props.img} alt={props.title} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
           {props.title}
-          <div className="badge bg-yellow-300 text-black"><span>{props.rating > 4 ? <FaStar /> : <FaRegStarHalfStroke />}</span>{props.rating}</div>
+          <div className="badge bg-yellow-300 text-black">
+            <span>
+              {props.rating > 4 ? <FaStar /> : <FaRegStarHalfStroke />}
+            </span>
+            {props.rating}
+          </div>
         </h2>
-        <div className="text-gray-500 card-actions justify-start text-xl">${props.price}</div>
-        <div className="card-actions justify-end mt-auto">
-            {tags}
+        <div className="text-gray-500 card-actions justify-start text-xl">
+          ${props.price}
         </div>
+        <div className="card-actions justify-end mt-auto">{tags}</div>
         <div className="card-actions justify-center w-full mt-auto">
-            <Link to={`/product/${props.id}`}  className="btn btn-primary w-full mt-5">View Details</Link>
+          <Link
+            to={`/product/${props.id}`}
+            className="btn btn-primary w-full mt-5"
+          >
+            View Details
+          </Link>
 
-
-            {cartbutton ? (
-            <button className="btn btn-error w-full mt-5" onClick={addToCart}>Remove from Cart</button>
-            ) : (
-            <button className="btn btn-secondary w-full mt-5" onClick={addToCart}>Add to Cart</button>
-            )}
-
-
-
-
-            
-            
+          {cartbutton ? (
+            <button className="btn btn-error w-full mt-5" onClick={addToCart}>
+              Remove from Cart
+            </button>
+          ) : (
+            <button
+              className="btn btn-secondary w-full mt-5"
+              onClick={addToCart}
+            >
+              Add to Cart
+            </button>
+          )}
         </div>
       </div>
     </div>
--- frontend/src/Pages/CartPage.jsx
+++ frontend/src/Pages/CartPage.jsx
@@ -1,65 +1,93 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import Cookies from "js-cookie";
 
 export default function CartPage() {
+  const [code, setCode] = useState([]);
 
-    const [count, setCount] = useState(1);
+  useEffect(() => {
+    const productCode = Cookies.get("id");
+    if (productCode) {
+      // setCode(productCode);
+      setCode([...new Set(productCode.split(","))]);
+    }
+  }, []);
 
-    const [data, setData] = useState([
-      {
-          title: "",
-          thumbnail: "",
-          price: "",
-          sku: ""
-      }
-    ]);
 
-    const fetchData = () => {
+  const [cart, setCart] = useState([]);
+
+  function fetchData() {
+    for (let i = 0; i < code.length; i++) {
       axios
-        .get(`http://192.168.0.105:8000/products/?sku=XNIH1MTA`)
+        .get(`http://192.168.0.105:8000/products/${code[i]}/`)
         .then((res) => {
-          setData(res.data[0]);
+          setCart((cart) => [...cart, res.data]);
         })
         .catch((err) => {
           console.log(err);
-        })
-    };
+        });
+    }
+  }
 
-    useEffect(() => {
-      fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, [code]);
 
-
-
-    const increase = () => {
-      setCount(count + 1);
-    };
   
-    const decrease = () => {
-      if (count > 1) {
-        setCount(count - 1);
-      }
-    };
 
+  console.log(cart);
 
+  const [count, setCount] = useState(1);
 
-  return (
-    <div className="mb-20">
-      <div className="h-screen pt-20">
-        <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
-        <div className="mx-auto w-full justify-center px-6 md:flex md:space-x-6 xl:px-0">
+  const [data, setData] = useState([
+    {
+      title: "",
+      thumbnail: "",
+      price: "",
+      sku: "",
+    },
+  ]);
+
+  // function fetchData(sku) {
+  //   axios
+  //     .get(`http://192.168.0.105:8000/products/?sku=${sku}`)
+  //     .then((res) => {
+  //       setData(res.data[0]);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     })
+  // };
+
+  // useEffect(() => {
+  //   fetchData("XNIH1MTA");
+  // }, []);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  const cartItems = cart.map((item) => {
+    return(
+
+      <div className="mx-auto w-full justify-center px-6 md:flex md:space-x-6 xl:px-0">
           <div className="rounded-lg md:w-2/3">
             <div className="justify-between mb-6 rounded-lg  p-6 bg-base-300 shadow-md sm:flex sm:justify-start">
               <img
-                src={data.thumbnail}
+                src={item.thumbnail}
                 alt="product-image"
                 className="w-full h-32 rounded-lg sm:w-40"
               />
               <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
                 <div className="mt-5 sm:mt-0">
-                  <h2 className="text-lg font-bold">{data.title}</h2>
-                  <p className="mt-1 text-s ">{data.price}</p>
+                  <h2 className="text-lg font-bold">{item.title}</h2>
+                  <p className="mt-1 text-s ">{item.price}</p>
                 </div>
                 <div className="mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                   <div className="flex items-center border-gray-100">
@@ -80,7 +108,7 @@ export default function CartPage() {
                     </span>
                   </div>
                   <div className="flex items-center space-x-4">
-                    <p className="text-lg">Total: ${count * data.price}</p>
+                    <p className="text-lg">Total: ${count * item.price}</p>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -100,6 +128,28 @@ export default function CartPage() {
               </div>
             </div>
           </div>
+
+        </div>
+    )
+  })
+  
+
+  return (
+    <div className="mb-20">
+      <div className="h-screen pt-20">
+        <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+        {cartItems}
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
 
           {/* <div className="mt-6 h-full rounded-lg border  p-6 shadow-md md:mt-0 md:w-1/3">
             <div className="mb-2 flex justify-between">
@@ -122,8 +172,3 @@ export default function CartPage() {
               Check out
             </button>
           </div> */}
-        </div>
-      </div>
-    </div>
-  );
-}
