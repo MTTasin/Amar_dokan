2208f288369ea66852fad96c386ac81ac75dd4f0
M.T. Tasin <m.t.tasin20@gmail.com>
2024-10-21 21:08:12 +0600

    Almost a cart system

---
+++ frontend/src/Context.jsx
@@ -0,0 +1,24 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+    const [cartItems, setCartItems] = useState([]);
+    
+    
+
+    return (
+        <Context.Provider
+            value={{
+                cartItems,
+                setCartItems,
+            }}
+        >
+            {children}
+        </Context.Provider>
+    );
+};
+
+export const useStateContext = () => {
+    return useContext(Context);
+};
